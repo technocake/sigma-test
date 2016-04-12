4d8ccdfc6546d6f7e44572d15fd0319e65fe6724
technocake <technocake@komsys.org>
2016-04-12 03:09:51 +0200

    Add no-result page

--- c-sharp.mm
+++ c-sharp.mm
@@ -7,7 +7,7 @@
 <node CREATED="1460412546098" ID="ID_1658545206" MODIFIED="1460412561316" TEXT="Field"/>
 <node CREATED="1460412551450" ID="ID_1766536604" MODIFIED="1460415923234" TEXT="Property" VSHIFT="-2"/>
 <node CREATED="1460412566913" ID="ID_21080033" MODIFIED="1460415973658" TEXT="Method" VSHIFT="-17"/>
-<node CREATED="1460412677107" ID="ID_939797896" LINK="https://msdn.microsoft.com/en-us/library/x9afc042.aspx" MODIFIED="1460415976311" VSHIFT="19">
+<node CREATED="1460412677107" ID="ID_939797896" LINK="https://msdn.microsoft.com/en-us/library/x9afc042.aspx" MODIFIED="1460421717635" VSHIFT="19">
 <richcontent TYPE="NODE"><html>
   <head>
     
@@ -20,7 +20,19 @@
       <font size="2">msdn.microsoft.com</font>
     </p>
   </body>
-</html></richcontent>
+</html>
+</richcontent>
+<node CREATED="1460421781867" ID="ID_1437221916" MODIFIED="1460421789422">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="gfx/comment.png" />
+  </body>
+</html>
+</richcontent>
+</node>
 </node>
 </node>
 </node>
